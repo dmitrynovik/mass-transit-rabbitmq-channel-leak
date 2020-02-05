095380a53f046bf6b5b733da8235cf62d292a57b
Dmitry Novik <dmitry.novik@gmail.com>
2020-02-05 15:48:50 +1100

    Original version

--- MassTrannsitChannelUsage/Program.cs
+++ MassTrannsitChannelUsage/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using MassTransit;
 
-namespace MassTrannsitChannelUsage
+namespace ChannelUsage
 {
     class Program
     {
-        static void Main(string[] args)
+        static IBusControl _serverBus;
+
+        public static async Task Main()
         {
+            _serverBus = Bus.Factory.CreateUsingRabbitMq(c =>
+            {
+                var host = c.Host("localhost", "/");
+
+                c.ReceiveEndpoint(host, "channel_test", ep => { ep.Handler<Message>(cx => cx.RespondAsync(new Response())); });
+            });
+
+            await _serverBus.StartAsync();
+            try
+            {
+                bool keepGoing = true;
+
+                Task.Run(() =>
+                {
+                    Console.Write("Press enter to exist");
+                    Console.ReadLine();
+                    keepGoing = false;
+                });
+
+                int index = 0;
+                while (keepGoing)
+                {
+                    await Task.Delay(100);
+
+                    Console.WriteLine("Request {0}", ++index);
+                    var client = Bus.Factory.CreateUsingRabbitMq(c =>
+                    {
+                        var host = c.Host("localhost", "/");
+                    });
+
+                    await client.StartAsync();
+                    try
+                    {
+                        var request = client.CreatePublishRequestClient<Message, Response>(TimeSpan.FromSeconds(30));
+                        await request.Request(new Message());
+                    }
+                    finally
+                    {
+                        await client.StopAsync();
+                    }
+                }
+            }
+            finally
+            {
+                await _serverBus.StopAsync();
+            }
         }
+    }
+
+    class Message
+    {
+    }
+
+    class Response
+    {
     }
 }
